5dee996f4b361114eb7c7c09ba2de9bdb652a0d4
Andreia Ricardo Cruz <kauacruzps@gmail.com>
2025-02-12 17:06:12 -0300

    Modularização da funcao salvar

---
+++ update.js
@@ -0,0 +1,19 @@
+
+export function salvar(index){
+    let dados = JSON.parse(localStorage.getItem('pessoas')) || []
+
+    dados[index] = {
+     nome : document.getElementById('nome').value,
+     tel : document.getElementById('tel').value,
+     estado : document.getElementById('estado').value,
+     cidade : document.getElementById('cidade').value
+    }
+
+    localStorage.setItem('pessoas', JSON.stringify(dados))
+
+    document.location.reload()
+}
+
+export function teste () {
+    console.log('aaaaa')
+}
